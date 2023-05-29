4e65020c44f33a342fb41cd1d0af93160cb2a6b6
Николай <colt92@inbox.ru>
2023-05-29 20:35:55 +0500

    Initial commit

---
+++ src/test/java/com/skypro/petsheltersbot/PetSheltersBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.petsheltersbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSheltersBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
