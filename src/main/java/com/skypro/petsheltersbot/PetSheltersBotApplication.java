d0408783919f8eaafd744f1292176452545012e4
Николай <colt92@inbox.ru>
2023-05-29 20:35:55 +0500

    Initial commit

---
+++ src/main/java/com/skypro/petsheltersbot/PetSheltersBotApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.petsheltersbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSheltersBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetSheltersBotApplication.class, args);
+    }
+
+}
