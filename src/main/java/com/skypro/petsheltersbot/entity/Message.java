e90e4a5d381b46d9fc54edd4e9361daa1ec17836
MakTub <7andreyru@mail.ru>
2023-06-07 21:40:17 +0500

    добавлены геттеры и сеттеры

--- src/main/java/com/skypro/petsheltersbot/entity/Message.java
+++ src/main/java/com/skypro/petsheltersbot/entity/Message.java
@@ -1,11 +1,14 @@
 package com.skypro.petsheltersbot.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "message", schema = "public", catalog = "telegram")
 public class Message {
