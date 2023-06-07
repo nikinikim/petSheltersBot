5419162a65bb3bfefab854a24c65c26129e67e03
Kristina Adigamova <kriwowak@yandex.ru>
2023-06-07 21:23:20 +0500

    Модели

--- src/main/java/com/skypro/petsheltersbot/entity/Cats.java
+++ src/main/java/com/skypro/petsheltersbot/entity/Cats.java
@@ -1,4 +1,61 @@
 package com.skypro.petsheltersbot.entity;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cats")
 public class Cats {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+    @Column(name = "breed", nullable = false)
+    private String breed;
+    @Column(name = "age", nullable = false)
+    private int age;
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
