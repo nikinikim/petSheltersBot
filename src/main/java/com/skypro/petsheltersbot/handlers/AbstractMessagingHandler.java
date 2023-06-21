44f12e5df99266e2ab200b3046cb440521945831
MakTub <7andreyru@mail.ru>
2023-06-21 21:23:45 +0500

    Этап 0 и Этап 1 (код почищен)

--- src/main/java/com/skypro/petsheltersbot/handlers/AbstractMessagingHandler.java
+++ src/main/java/com/skypro/petsheltersbot/handlers/AbstractMessagingHandler.java
@@ -1,7 +1,6 @@
 package com.skypro.petsheltersbot.handlers;
 
 import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.model.Update;
 
 public abstract class AbstractMessagingHandler implements TelegramHandler {
     protected TelegramBot telegramBot;
@@ -9,8 +8,4 @@ public abstract class AbstractMessagingHandler implements TelegramHandler {
     public AbstractMessagingHandler(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
     }
-
-    //public abstract void handlerUpdateCat(Update update);
-
-    //public abstract void handlerUpdateDog(Update update);
 }
--- src/main/java/com/skypro/petsheltersbot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/skypro/petsheltersbot/listener/TelegramBotUpdatesListener.java
@@ -4,23 +4,15 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
-import com.skypro.petsheltersbot.handlers.StartHandler;
 import com.skypro.petsheltersbot.handlers.TelegramHandler;
-import liquibase.pro.packaged.S;
+import com.skypro.petsheltersbot.repositories.InfoMessageRepository;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import com.skypro.petsheltersbot.entity.InfoMessage;
-import com.skypro.petsheltersbot.repositories.InfoMessageRepository;
 
 import javax.annotation.PostConstruct;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -57,22 +49,6 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             else {
                 handlerUpdatePet(data, update.callbackQuery().from().id());
             }
-
-            /*String data = update.callbackQuery().data();
-            if (data.equals("/Cats")) {
-                for (TelegramHandler handler : handlers) {
-
-                        handler.handlerUpdateCat(update);
-                        break;
-System.out.println("cool");
-                }
-                handlerUpdateCat(update);
-
-            }
-            if (data.equals("/Dogs")) {
-                handlerUpdateDogs(update);
-System.out.println("cooool");
-            }*/
         } else {
             for (TelegramHandler handler : handlers) {
                 if (handler.appliesTo(update)) {
@@ -86,8 +62,6 @@ System.out.println("cooool");
     }
 
     private void handlerUpdatePet(String data, Long fromId) {
-        // if (update.callbackQuery() != null) {
-        //String data = update.callbackQuery().data();
         if (data.contains("/Информация_о_приюте")) {
             this.telegramBot.execute(new SendMessage(fromId, String.format("Приют для %s", data.startsWith("/Cats") ? "кошек. Основан..." : "собак. Основан...")));
         }
@@ -107,69 +81,6 @@ System.out.println("cooool");
             this.telegramBot.execute(new SendMessage(fromId, String.format("Вызываю волонтера из приюта для %s", data.startsWith("/Cats") ? "кошек. Подождите..." : "собак. Подождите...")));
         }
     }
-
-    /*private void handlerUpdateCat(Update update) {
-        if (update.callbackQuery() != null) {
-            String data = update.callbackQuery().data();
-            if (data.equals("/Информация_о_приюте")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вывод: Информация о приюте"));
-            }
-            if (data.equals("/CONTACTINFO")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вывод: Расписание работы приюта, адрес, схема проезда"));
-            }
-            if (data.equals("/REGISTRATIONOFCARPASS")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вывод: Контактные данные охраны для оформления пропуска на машину"));
-            }
-            if (data.equals("/SAFETYSHELTER")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вывод: Общие рекомендации о технике безопасности на территории приюта"));
-            }
-            if (data.equals("/LEAVECONTACTDETAILS")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вывод: Оставить контактные данные для связи"));
-            }
-            if (data.equals("/Позвать_волонтера")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вызываю волонтера. Подождите..."));
-            }
-        } else {
-            for (TelegramHandler handler : handlers) {
-                if (handler.appliesTo(update)) {
-                    handler.handlerUpdateCat(update);
-                    break;
-                }
-            }
-        }
-    }
-
-    private void handlerUpdateDogs(Update update) {
-        if (update.callbackQuery() != null) {
-            String data = update.callbackQuery().data();
-            if (data.equals("/Информация_о_приюте")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вывод: Информация о приюте"));
-            }
-            if (data.equals("/CONTACTINFO")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вывод: Расписание работы приюта, адрес, схема проезда"));
-            }
-            if (data.equals("/REGISTRATIONOFCARPASS")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вывод: Контактные данные охраны для оформления пропуска на машину"));
-            }
-            if (data.equals("/SAFETYSHELTER")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вывод: Общие рекомендации о технике безопасности на территории приюта"));
-            }
-            if (data.equals("/LEAVECONTACTDETAILS")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вывод: Оставить контактные данные для связи"));
-            }
-            if (data.equals("/Позвать_волонтера")) {
-                this.telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), "Вызываю волонтера. Подождите..."));
-            }
-        } else {
-            for (TelegramHandler handler : handlers) {
-                if (handler.appliesTo(update)) {
-                    handler.handlerUpdate(update);
-                    break;
-                }
-            }
-        }
-    }*/
-
     public void sendMessage(Long chatId, String message) {
         telegramBot.execute(new SendMessage(chatId, message));
     }
